f4fe913fe7788954158c0d254cf332d6af1482fc
Maxtopher <maxwellchristopher@college.harvard.edu>
2021-07-25 20:10:45 -0400

    10/final

---
+++ 10_Machine/Stepper_Motor_Huzzah_Trial/Stepper_Motor_Huzzah_Trial.ino
@@ -0,0 +1,24 @@
+
+#include <AccelStepper.h>
+
+const int stepPin = 13;  // blue
+const int dirPin = 12;   // orange
+
+// Define a stepper and the pins it will use
+AccelStepper stepper(1, stepPin, dirPin); // initialise accelstepper for a two wire board
+
+void setup()
+{
+}
+
+void loop()
+{
+  if (stepper.distanceToGo() == 0)
+  {
+    delay(1000);
+    stepper.moveTo(rand() % 1000);
+    stepper.setMaxSpeed((rand() % 1000) + 1);
+    stepper.setAcceleration((rand() % 1000) + 1);
+  }
+  stepper.run();
+}
